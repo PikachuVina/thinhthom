31c9ae745843595cba04a85af04079333553ae71
PikachuVina <bmn.2312@gmail.com>
2017-07-31 07:40:59 +0700

    1

--- Menu.php
+++ Menu.php
@@ -31,7 +31,7 @@ if ($_GET['type'] === 'delete') {
 	        {
 				"type": "web_url",
 				"title": "Contact Info",
-				"url": "http://thien-it.me",
+				"url": "http://nghĩa.vn",
 				"webview_height_ratio": "full"
 	        }
 	      ]
